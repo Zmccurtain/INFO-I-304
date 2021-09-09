d6b9bcf46b5dccd4ee51cfbe4b23e51e6bcb1e2f
Zmccurtain <zmccurtain2@gmail.com>
2021-09-08 21:30:04 -0400

    Week 3 done

--- Projects/Unit_1/Assets/Scripts/Car_Controller.cs
+++ Projects/Unit_1/Assets/Scripts/Car_Controller.cs
@@ -6,7 +6,7 @@ public class Car_Controller: MonoBehaviour
 {
     public float speed;
     public float maxSpeed;
-
+    public float nitroFactor;
     private Rigidbody rb;
     // Start is called before the first frame update
     void Start()
@@ -14,22 +14,25 @@ public class Car_Controller: MonoBehaviour
         rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        float target = 0;
-        if (Input.GetKey(KeyCode.W))
-        {
-            target = maxSpeed;
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            rb.AddTorque(transform.up);
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            rb.AddTorque(transform.up*-1);
-        }
+        Nitro();
+    }
+    void FixedUpdate()
+    {
+        Movement();
+    }
+    void Nitro()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) { speed *= nitroFactor; maxSpeed *= nitroFactor; }
+        if (Input.GetKeyUp(KeyCode.Space)) { speed /= nitroFactor; maxSpeed /= nitroFactor; }
+    }
+    void Movement()
+    {
+        var target = maxSpeed * Input.GetAxisRaw("Vertical");
+        var torque = Input.GetAxisRaw("Horizontal");
+        rb.AddTorque(transform.up * torque * speed * .4f);
+
         Vector3 force = Mathf.Clamp(target - rb.velocity.z, -speed, speed) * transform.forward;
         rb.AddForce(force);
     }
